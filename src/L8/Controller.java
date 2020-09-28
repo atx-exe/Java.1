36b125748c2c6a8309a9b30c2e6689e3c8119c50
i.nefedov <atx-exe@mail.ru>
2020-09-28 23:48:18 +0300

    L8 - Start

---
+++ src/L8/Controller.java
@@ -0,0 +1,12 @@
+package L8;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void setGameScene(ActionEvent actionEvent) {
+
+    }
+
+    public void setInfoScene(ActionEvent actionEvent) {
+    }
+}
--- src/L8/Main.java
+++ src/L8/Main.java
@@ -1,4 +1,27 @@
 package L8;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent menu = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Parent game = FXMLLoader.load(getClass().getResource("game.fxml"));
+        Parent info = FXMLLoader.load(getClass().getResource("info.fxml"));
+        primaryStage.setTitle("XO Game");
+        primaryStage.setResizable(false);
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(300);
+        primaryStage.setScene(new Scene(menu));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
