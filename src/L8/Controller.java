dcd3964a5203fb931523b25dd393dbc8c944fcc3
i.nefedov <atx-exe@mail.ru>
2020-09-29 23:06:24 +0300

    L8 - Add Buttons

--- src/L8/Controller.java
+++ src/L8/Controller.java
@@ -1,12 +1,20 @@
 package L8;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
 public class Controller {
-    public void setGameScene(ActionEvent actionEvent) {
+    public Label jlbMenu;
 
+    public void onActPvP(ActionEvent actionEvent) {
+        jlbMenu.setText("Set PvP");
     }
 
-    public void setInfoScene(ActionEvent actionEvent) {
+    public void onActPvPC(ActionEvent actionEvent) {
+        jlbMenu.setText("Set PvPC");
+    }
+
+    public void onActPCvPC(ActionEvent actionEvent) {
+        jlbMenu.setText("Set PCvPC");
     }
 }
--- src/L8/Main.java
+++ src/L8/Main.java
@@ -9,14 +9,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent menu = FXMLLoader.load(getClass().getResource("menu.fxml"));
         Parent game = FXMLLoader.load(getClass().getResource("game.fxml"));
-        Parent info = FXMLLoader.load(getClass().getResource("info.fxml"));
         primaryStage.setTitle("XO Game");
         primaryStage.setResizable(false);
         primaryStage.setHeight(400);
         primaryStage.setWidth(300);
-        primaryStage.setScene(new Scene(menu));
+        primaryStage.setScene(new Scene(game));
         primaryStage.show();
     }
 
