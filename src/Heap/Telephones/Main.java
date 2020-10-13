fa4b89732ce77ffc8fc71e0f948cf40e2dd128d9
i.nefedov <atx-exe@mail.ru>
2020-10-13 13:55:54 +0300

    L8.Exam

--- src/Heap/Telephones/Main.java
+++ src/Heap/Telephones/Main.java
@@ -8,25 +8,33 @@ public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
         Phone[] groupPhones = {new Phone("Phone 1"), new Phone("Phone 2"), new Phone("Phone 3")};
-        long[] referenceNumbers = {89991001010L, 89991001011L, 89991001012L, 89991001013L, 89991001014L, 89991001020L,
-                89991001021L, 89991001023L, 89991001110L, 89991001111L, 89991001112L, 89991001120L, 89991002010L,
-                89991002011L, 89991002030L, 89991011010L, 89991011010L, 89991011010L, 89991011040L, 89991021042L,
-                89991021243L, 89991021314L, 89991021310L, 89991021315L, 89991021317L, 89991021410L, 89991031412L,
-                89991031510L, 89991031514L, 89991031610L, 89991031613L, 89991031614L, 89991031650L, 89991031710L,
-                89991031712L, 89991031714L, 89991031715L, 89991041010L, 89991041313L, 89991041510L
+        long[] referenceNumbers = {
+                89991001010L, 89991001011L, 89991001012L, 89991001013L, 89991001014L, 89991001020L, 89991001021L,
+                89991001023L, 89991001110L, 89991001111L, 89991001112L, 89991001120L, 89991002010L, 89991002011L,
+                89991002030L, 89991011010L, 89991011010L, 89991011010L, 89991011040L, 89991021042L, 89991021243L,
+                89991021314L, 89991021310L, 89991021315L, 89991021317L, 89991021410L, 89991031412L, 89991031510L,
+                89991031514L, 89991031610L, 89991031613L, 89991031614L, 89991031650L, 89991031710L, 89991031712L,
+                89991031714L, 89991031715L, 89991041010L, 89991041313L, 89991041510L, 89991050012L, 89991050045L
         };
         int idReferenceNumbers = 0;
 
         for (int i = 0; i < 100; i++) {
-            switch (rnd.nextInt(3)) {
+            switch (rnd.nextInt(4)) {
                 case (0):
-                    do {
+                    boolean allPhonesAreBusy = true;
+                    for (Phone phone : groupPhones) {
+                        if (!phone.isBusy()) {
+                            allPhonesAreBusy = false;
+                            break;
+                        }
+                    }
+                    while (!allPhonesAreBusy) {
                         int randomPhone = rnd.nextInt(groupPhones.length);
                         if (!groupPhones[randomPhone].isBusy()) {
                             groupPhones[randomPhone].incomingCall(89800000000L + i);
                             break;
                         }
-                    } while ((groupPhones[0].isBusy() && groupPhones[1].isBusy() && groupPhones[2].isBusy()));
+                    }
                     break;
                 case (1):
                     for (Phone phone : groupPhones) {
--- src/L8/Exam/Controller.java
+++ src/L8/Exam/Controller.java
@@ -10,11 +10,17 @@ public class Controller {
     public GridPane gpMap;
     public Button bt00;
     public Button bt01;
+    public Button bt02;
+    public Button bt10;
+    public Button bt11;
+    public Button bt12;
+    public Button bt20;
+    public Button bt21;
+    public Button bt22;
 
     public void onActPvP(ActionEvent actionEvent) {
         jlbMenu.setText("Set PvP");
         new Game(Player.Human, Player.Human);
-
     }
 
     public void onActPvPC(ActionEvent actionEvent) {
@@ -27,9 +33,41 @@ public class Controller {
         new Game(Player.PC, Player.PC);
     }
 
-    public void btMap(ActionEvent actionEvent) {
-        bt00.setText(bt00.getId());
-//        bt01.setBackground(Color.AQUA);
-        bt01.setText(bt01.getId());
+    public void bt00(ActionEvent actionEvent) {
+        bt00.setText("X");
+        bt00.setDisable(true);
+    }
+
+    public void bt01(ActionEvent actionEvent) {
+        bt01.setText("X");
+        bt01.setDisable(true);
+    }
+    public void bt02(ActionEvent actionEvent) {
+        bt02.setText("X");
+        bt02.setDisable(true);
+    }
+    public void bt10(ActionEvent actionEvent) {
+        bt10.setText("X");
+        bt10.setDisable(true);
+    }
+    public void bt11(ActionEvent actionEvent) {
+        bt11.setText("X");
+        bt11.setDisable(true);
+    }
+    public void bt12(ActionEvent actionEvent) {
+        bt12.setText("X");
+        bt12.setDisable(true);
+    }
+    public void bt20(ActionEvent actionEvent) {
+        bt20.setText("X");
+        bt20.setDisable(true);
+    }
+    public void bt21(ActionEvent actionEvent) {
+        bt21.setText("X");
+        bt21.setDisable(true);
+    }
+    public void bt22(ActionEvent actionEvent) {
+        bt22.setText("X");
+        bt22.setDisable(true);
     }
 }
